817d0c766c04a385561315b5ece072e804f8a0d9
Alissa-1 <103644741+Alissa-1@users.noreply.github.com>
2022-05-29 14:29:15 +0300

    Change in String 4 CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(double money, double years, double percent) {
         double months = years * 12;
-        double monthPercent = percent / months / 100;
+        double monthPercent = percent / 12 / 100;
         double K = Math.pow(1 + monthPercent, months);
         double annualK = (monthPercent * K / (K - 1));
         double annualPayment = money * annualK;
@@ -11,3 +11,4 @@ public class CreditPaymentService {
     }
 
 }
+
